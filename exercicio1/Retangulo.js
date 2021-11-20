7b04b6563c0d293e696a52c210b983eb194ac82f
romuloLadeira <romulo.alesandro@gmail.com>
2021-11-19 23:41:28 -0300

    exercicio1

---
+++ exercicio1/Retangulo.js
@@ -0,0 +1,14 @@
+class Retangulo{
+    #largura
+    #altura
+    constructor(largura, altura)
+    {
+        this.#largura = largura
+        this.#altura = altura 
+    }
+}
+const ret1 = new Retangulo(5,6)
+const ret2 = new Retangulo(7,2)
+const ret3 = new Retangulo(2,4)
+
+const arrayRetangulos = [ret1, ret2, ret3] 
