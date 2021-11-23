20419293f81051fc29d8d363cc0a0a4f2c913d54
romuloLadeira <romulo.alesandro@gmail.com>
2021-11-23 15:09:06 -0300

    exercicio-3

---
+++ exercicio3/scripts/Endereco.js
@@ -0,0 +1,29 @@
+class Endereco{
+    #logradouro
+    #numero
+    #cidade
+    #estado
+    #pais
+    #cep
+    constructor(logradouro, numero, cidade, estado, pais,cep ){
+        const STR = 'string'
+        
+        if(
+            typeof logradouro !== STR ||
+            typeof numero !== STR ||
+            typeof cidade !== STR ||
+            typeof estado !== STR ||
+            typeof pais !== STR ||
+            typeof cep !== STR 
+            ){
+               throw  'Endereço invalido'
+            }
+
+        this.#logradouro = logradouro
+        this.#numero = numero
+        this.#cidade = cidade
+        this.#estado = estado
+        this.#pais = pais
+        this.#cep = cep
+    }
+}
